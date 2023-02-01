1e6b5c772623eeadbe454db2fadf2075bda207cb
jangsujin00 <32193870@dankook.ac.kr>
2023-02-01 12:28:17 +0900

    Final

--- face_recognition_knn_trainer.py
+++ face_recognition_knn_trainer.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 This is an example of using the k-nearest-neighbors(knn) algorithm for face recognition.
 
@@ -30,7 +30,7 @@ from math import sqrt
 from sklearn import neighbors
 from os import listdir
 from os.path import isdir, join, isfile, splitext
-import pickle
+import pickle       #  pickle 모듈 사용. 파이썬에서 만들어지는 것은 뭐든지 다 파일에 적을 수 있음
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 import face_recognition
 from face_recognition import face_locations
@@ -40,13 +40,15 @@ import cv2
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-def train(train_dir, model_save_path = "", n_neighbors = None, knn_algo = 'ball_tree', verbose=False):
+def train(train_dir, model_save_path="", n_neighbors=None, knn_algo='ball_tree', verbose=False):        # Ball-Tree는 범위를 기준으로 차원을 내림차순 정렬한 후, 서브트리에 속한 범위의 반을 기준으로 KD-Tree를 적용
+        # 디렉토리, disk 모델 저장 경로, 분류에 가중치를 부여할 이웃 수, 기본 데이터 구조, 진행상황 보여줌 - x )                                                               # knn : K-Nearest Neighbors ('K개의 근접 이웃')
     """
     Trains a k-nearest neighbors classifier for face recognition.
 
-    :param train_dir: directory that contains a sub-directory for each known person, with its name.
+    :param train_dir: directory that contains a sub-directory for each known person, with its name.     # 이름과 함께 알려진 각 사람에 대한 하위 디렉토리를 포함하는 디렉토리.
 
      (View in source code to see train_dir example tree structure)
+     (train_dir 예제 트리 구조를 보려면 소스 코드에서 보기)
 
      Structure:
         <train_dir>/
@@ -58,85 +60,89 @@ def train(train_dir, model_save_path = "", n_neighbors = None, knn_algo = 'ball_
         │   ├── <somename1>.jpeg
         │   └── <somename2>.jpeg
         └── ...
-    :param model_save_path: (optional) path to save model of disk
-    :param n_neighbors: (optional) number of neighbors to weigh in classification. Chosen automatically if not specified.
-    :param knn_algo: (optional) underlying data structure to support knn.default is ball_tree
+    :param model_save_path: (optional) path to save model of disk           # disk 모델 저장 경로
+    :param n_neighbors: (optional) number of neighbors to weigh in classification. Chosen automatically if not specified.   # 분류에 가중치를 부여할 이웃 수. 지정하지 않으면 자동으로 선택
+    :param knn_algo: (optional) underlying data structure to support knn.default is ball_tree           # knn.default를 지원하는 기본 데이터 구조는 ball_tree
     :param verbose: verbosity of training
-    :return: returns knn classifier that was trained on the given data.
+    :return: returns knn classifier that was trained on the given data.             # 주어진 데이터가 학습되는 knn 분류기 반환
     """
     X = []
     y = []
-    for class_dir in listdir(train_dir):
-        if not isdir(join(train_dir, class_dir)):
+    for class_dir in listdir(train_dir):            # dir( ) [directory] : 해당 객체가 어떤 변수와 메소드(method)를 가지고 있는지 나열
+        if not isdir(join(train_dir, class_dir)):   # isdir() : 디렉토리 존재 여부 확인 - TRUE/False로 반환      # join() : 리스트를 문자열로 합쳐줌
             continue
-        for img_path in sorted(glob(join(train_dir, class_dir, '*'))):
+        for img_path in sorted(glob(join(train_dir, class_dir, '*'))):          # glob() : 인자로 받은 패턴과 이름이 일치하는 모든 파일과 디렉터리의 리스트를 반환 (패턴을 그냥 *라고 주면 모든 파일과 디렉터리)
             image = face_recognition.load_image_file(img_path)
             faces_bboxes = face_locations(image)
             if len(faces_bboxes) != 1:
-                if verbose:
-                    print("image {} not fit for training: {}".format(img_path, "didn't find a face" if len(faces_bboxes) < 1 else "found more than one face"))
+                if verbose:                 # 학습 진행 상황 보여주는
+                    print("image {} not fit for training: {}".format(img_path, "didn't find a face" if len(
+                        faces_bboxes) < 1 else "found more than one face"))
                 continue
             X.append(face_recognition.face_encodings(image, known_face_locations=faces_bboxes)[0])
             y.append(class_dir)
 
-
     if n_neighbors is None:
-        n_neighbors = int(round(sqrt(len(X))))
+        n_neighbors = int(round(sqrt(len(X))))      # round:반올림(sqrt:제곱근( X의 길이 ))
         if verbose:
             print("Chose n_neighbors automatically as:", n_neighbors)
 
-    knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
+    knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')       # clf (Classifier) 분류기
     knn_clf.fit(X, y)
 
     if model_save_path != "":
-        with open(model_save_path, 'wb') as f:
-            pickle.dump(knn_clf, f)
+        with open(model_save_path, 'wb') as f:      # model_save_path 파일을 새로 열어서 f라는 이름을 붙임. wb는 바이트(byte) 형식으로 쓰겠다(write)의 의미
+            pickle.dump(knn_clf, f)                 # dump는 knn_clf를 파일 f에 기록. (파이썬이 실행되는 경로에 knn_clf파일이 만들어짐)
     return knn_clf
 
-def predict(X_img_path, knn_clf = None, model_save_path ="", DIST_THRESH = .4):
-    """
+
+def predict(X_img_path, knn_clf=None, model_save_path="", DIST_THRESH=.4):      # 거리 한계점 (distance_threshold= .4) - 숫자 클수록 학습된 사람으로 잘못 분류할 가능성 큼
+    """    # 인식된 이미지 경로, knn 분류자 객체, knn 분류자의 경로, 거리 한계점
     recognizes faces in given image, based on a trained knn classifier
 
     :param X_img_path: path to image to be recognized
-    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
-    :param model_save_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
-    :param DIST_THRESH: (optional) distance threshold in knn classification. the larger it is, the more chance of misclassifying an unknown person to a known one.
-    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
+    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.    # knn 분류자 객체. 지정하지 않으면 model_save_path를 지정해야 함
+    :param model_save_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.  # pickled knn 분류자의 경로. 지정되지 않은 경우 model_save_path는 knn_clf여야 함.
+    :param DIST_THRESH: (optional) distance threshold in knn classification. the larger it is, the more chance of misclassifying an unknown person to a known one.  # 크기가 클수록 알려지지 않은 사람을 알려진 사람으로 잘못 분류할 가능성이 커짐.
+    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...]. # 이미지에서 인식된 얼굴의 이름 및 위치 리스트 반환
         For faces of unrecognized persons, the name 'N/A' will be passed.
     """
 
-    if not isfile(X_img_path) or splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
-        raise Exception("invalid image path: {}".format(X_img_path))
+    if (not isfile(X_img_path)) or (splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS):   # not 연산자가 or 연산자보다 먼저
+        raise Exception("invalid image path: {}".format(X_img_path))                          # raise Exception : 오류 발생시 구문 출력
 
     if knn_clf is None and model_save_path == "":
-        raise Exception("must supply knn classifier either thourgh knn_clf or model_save_path")
-
+        raise Exception("must supply knn classifier either thourgh knn_clf or model_save_path")     # raise Exception : 오류 발생시 구문 출력
+                        # knn_clf나 model_save_path를 통해 knn 분류자를 제공해야 함.
     if knn_clf is None:
         with open(model_save_path, 'rb') as f:
             knn_clf = pickle.load(f)
 
-    X_img = face_recognition.load_image_file(X_img_path) # rgb
-    print (X_img.shape)
+    X_img = face_recognition.load_image_file(X_img_path)  # 3개의 RGB
+    print(X_img.shape)      #(가로, 세로, 3)
     # show = cv2.cvtColor(X_img, cv2.COLOR_RGB2BGR)
     # cv2.imshow('show', show)
     # cv2.waitKey()
-    X_faces_loc = face_locations(X_img) # (top, right, bottom, left)
+    X_faces_loc = face_locations(X_img)  # (top, right, bottom, left)
     if len(X_faces_loc) == 0:
         return []
-    print (X_faces_loc[0])
-    start = cv2.getTickCount()
+    print(X_faces_loc[0])           # (top, right, bottom, left) : 얼굴 좌표
+    start = cv2.getTickCount()              # GetTickCount() 함수는 마이크로 초마다 시간값 받아옴.
     faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_faces_loc)
-    
+
     closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
     time = (cv2.getTickCount() - start) / cv2.getTickFrequency() * 1000
-    print('prediction time: %.2fms'%time)
+    print('prediction time: %.2fms' % time)
     is_recognized = [closest_distances[0][i][0] <= DIST_THRESH for i in range(len(X_faces_loc))]
 
     # predict classes and cull classifications that are not with high confidence
-    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_faces_loc, is_recognized)]
+    # 신뢰도가 높지 않은 클래스 및 선별 분류를 예측
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in
+            zip(knn_clf.predict(faces_encodings), X_faces_loc, is_recognized)]
+
 
 def draw_preds(img_path, preds):
-    """
+    """     # 인식된 이미지 경로, 예측 결과
     shows the face recognition results visually.
 
     :param img_path: path to image to be recognized
@@ -145,21 +151,26 @@ def draw_preds(img_path, preds):
     """
     source_img = Image.open(img_path).convert("RGBA")
     draw = ImageDraw.Draw(source_img)
-    for pred in preds:
+    for pred in preds:               # 반복문으로 리스트의 모든 내용 출력    # ex) item_list = [1, 2, 3, 4, 5]
+                                                                        #     for item in item_list:
+                                                                        #        print(item)
         loc = pred[1]
         name = pred[0]
         # (top, right, bottom, left) => (left,top,right,bottom)
-        draw.rectangle(((loc[3], loc[0]), (loc[1],loc[2])), outline="red")
+        draw.rectangle(((loc[3], loc[0]), (loc[1], loc[2])), outline="red")
         # draw.text((loc[3], loc[0] - 30), name, font=ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 30))
         draw.text((loc[3], loc[0] - 21), name, font=ImageFont.truetype('./BMDOHYEON_TTF.TTF', 20))
     source_img.show()
 
+
 if __name__ == "__main__":
-    knn_clf = train("./data/train", model_save_path='./models/fr_knn.pkl')
+    knn_clf = train("./data/train", model_save_path='./models/fr_knn.pkl')  # train폴더에 학습시킬 이미지 폴더로 저장해야 나중에 라벨링 처리됨
     for img_path in listdir("./data/test"):
-        
         # preds = predict(join("./data/test", img_path), knn_clf=knn_clf)
-        preds = predict(join("./data/test", img_path), model_save_path='./models/fr_knn.pkl', DIST_THRESH=0.4)
-        
+        preds = predict(join("./data/test", img_path), model_save_path='./models/fr_knn.pkl', DIST_THRESH=0.4)      # 거리 한계점 (distance_threshold= 0.4)
+                        #       인식된 이미지 경로      ,             knn 분류자의 경로         ,    거리 한계점
         print(os.path.basename(img_path), preds)
         # draw_preds(join("./data/test", img_path), preds)
+
+# predict(X_img_path, knn_clf=None, model_save_path="", DIST_THRESH=.4):
+        # 인식된 이미지 경로, knn 분류자 객체, knn 분류자의 경로, 거리 한계점
--- main.py
+++ main.py
@@ -1,78 +1,97 @@
 from __future__ import print_function
 import numpy as np
 import face_recognition
+# dlib 설치가 안되서 구글링으로 https://github.com/Daiera/some_Resources/blob/master/dlib-19.17.0-cp37-cp37m-win_amd64.whl 검색후 파일 다운
+# 다운 파일 파이참 폴더로 옮기고 pip install dlib-19.17.0-cp37-cp37m-win_amd64.whl     # pip install face_recognition 설치
 import argparse
 import cv2
-import os
 import pickle
-from PIL import Image, ImageFont, ImageDraw, ImageEnhance
+from PIL import Image, ImageFont, ImageDraw, ImageEnhance       # PIL : Python Imaging Library
+import os
+import sklearn.neighbors._base
+from sklearn.neighbors import KNeighborsClassifier
 
-parser = argparse.ArgumentParser()
+
+# argparse : Python Script 실행 시, 하나의 Script 동작을 여러가지 상황에 따라 다르게 동작하도록 (여러 개 데이터(정해지지 않은 데이터)를 다루고 싶을 때)
+parser = argparse.ArgumentParser()    # 파서(parser) : 그 문장의 구조를 알아내는 구문 분석(parsing)
 parser.add_argument('--with_draw', help='do draw?', default='True')
+# 인수명을 'arg'와 같이 일반적으로 지정하면 arg가 된다.
 args = parser.parse_args()
-
-net = cv2.dnn.readNetFromCaffe('./models/deploy.prototxt.txt', './models/res10_300x300_ssd_iter_140000.caffemodel')
-
+#  dnn 모듈에서의 readNetFromCaffe 함수를 사용해, caffeframework의 네트워크 모델을 읽어들인다. -> (정의한 caffemodel과 prototxt를 인자로 받아와서) 하나의 Net 객체를 반환하게 된다.
+net = cv2.dnn.readNetFromCaffe('./models/deploy.prototxt.txt', './models/res10_300x300_ssd_iter_140000.caffemodel')     #DNN은 입력층(input layer)과 출력층(output layer) 사이에 여러 개의 은닉층(hidden layer)들로 이뤄진 인공신경망(Artificial Neural Network, ANN)
 knn_clf = pickle.load(open('./models/fr_knn.pkl', 'rb'))
 
-def adjust_gamma(image, gamma=1.0):
-    # build a lookup table mapping the pixel values [0, 255] to
-    # their adjusted gamma values
-    invGamma = 1.0 / gamma
+def adjust_gamma(image, gamma=1.0):  # 감마 조정
+    # build a lookup table mapping the pixel values [0, 255] to - 픽셀 값 [0, 255]를 매핑하는 조회 테이블을 작성
+    invGamma = 1.0 / gamma        # their adjusted gamma values - 조정된 감마 값
     table = np.array([((i / 255.0) ** invGamma) * 255
-        for i in np.arange(0, 256)]).astype("uint8")
- 
-    # apply gamma correction using the lookup table
+                      for i in np.arange(0, 256)]).astype("uint8")
+
+    # apply gamma correction using the lookup table - 조회 테이블 사용하여 감마 보정 적용
     return cv2.LUT(image, table)
 
-def preprocess(img):
+
+def preprocess(img):  # 이미지 전처리
     ### analysis
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     for i in range(1):
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         if gray_img.mean() < 130:
-            img = adjust_gamma(img, 1.5)
+            img = adjust_gamma(img, 1.5)  # 감마 조정
         else:
             break
     return img
 
+
 # vc = cv2.VideoCapture('./data/TAEYANG_ONLY_LOOK_AT_ME_MV.mp4')
-vc = cv2.VideoCapture('./data/gd_and_ty.mp4')
+cap = cv2.VideoCapture('./data/with_Maeng_1.mp4')  # 사용할 동영상 넣으면 결과로 이 영상에서 모자이크 처리와 라벨링 보여줌
+# 비디오 캡쳐. 동영상을 cap에 저장. 원 코드 vc를 cap으로 이름만 바꿔줌
 
-length = int(vc.get(cv2.CAP_PROP_FRAME_COUNT))
-print ('length :', length)
 
-if args.with_draw == 'True':
-    cv2.namedWindow('show', 0)
+# 동영상 저장하는 코드 [추가]
+width = int(cap.get(3))  # 가로 길이 가져오기
+height = int(cap.get(4))  # 세로 길이 가져오기
+fps = 20  # 초당 프레임
+fcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')  # VideoWriter_fourcc('M', 'J', 'P', 'G'): 모션 JPEG 코덱/ 코덱은 지정하는 함수
+out = cv2.VideoWriter('output_label.avi', fcc, fps, (width, height))  # '저장할 파일명' - 라벨링
+out_2 = cv2.VideoWriter('output_blur.avi', fcc, fps, (width, height))   # '저장할 파일명' - 블러
+
+length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+print('length :', length)
+
+# 라벨링 영상 불러오는 if문 namedWindow('show', 0)
+# if args.with_draw == 'True':
+#     cv2.namedWindow('show', 0)
 
 for idx in range(length):
-    img_bgr = vc.read()[1]
+    img_bgr = cap.read()[1]
     if img_bgr is None:
         break
     # if idx%3 != 0: continue
     # if idx < 200: continue
-    
-    start = cv2.getTickCount()
-    
+
+    start = cv2.getTickCount()  # getTickCount : OS부팅할 때부터 지나간 시간을 msec 단위로 돌려주는 함수
+
     ### preprocess
     img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
     img_bgr_ori = img_bgr.copy()
     img_bgr = preprocess(img_bgr)
 
     ### detection
-    border = (img_bgr.shape[1] - img_bgr.shape[0])//2
+    border = (img_bgr.shape[1] - img_bgr.shape[0]) // 2
+    # copyMakeBorder : 이미지를 액자 형태로 만들 때 사용. 가장자리가 추가되는데, 이 함수의 6번째 인자에 따라 추가되는 가장자리의 형태가 결정
     img_bgr = cv2.copyMakeBorder(img_bgr,
-                                 border, # top
-                                 border, # bottom
-                                 0, # left
-                                 0, # right
-                                 cv2.BORDER_CONSTANT,
-                                 value=(0,0,0))
+                                 border,  # top
+                                 border,  # bottom
+                                 0,  # left
+                                 0,  # right
+                                 cv2.BORDER_CONSTANT,  # 일정한 색상의 테두리를 추가
+                                 value=(0, 0, 0))
 
     (h, w) = img_bgr.shape[:2]
-    
+    # blob= 이미지, 사운드, 비디오와 같은 멀티미디어 데이터를 다룰 때 사용하는 라이브러리
     blob = cv2.dnn.blobFromImage(cv2.resize(img_bgr, (300, 300)), 1.0,
-        (300, 300), (104.0, 177.0, 123.0))
+                                 (300, 300), (104.0, 177.0, 123.0))
+    # 영상으로부터 블롭을 생성합니다. 이 함수는 입력 image로부터 4차원 블롭 객체를 생성하여 반환
     net.setInput(blob)
     detections = net.forward()
 
@@ -83,61 +102,106 @@ for idx in range(length):
     for i in range(0, detections.shape[2]):
         confidence = detections[0, 0, i, 2]
         if confidence < 0.6:
-                continue
+            continue
+        # astype메서드 : 열의 요소의 dtype을 변경하는함수
         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        (l, t, r, b) = box.astype("int") # l t r b
-        
-        original_vertical_length = b-t
-        t = int(t + (original_vertical_length)*0.15) - border
-        b = int(b - (original_vertical_length)*0.05) - border
+        (l, t, r, b) = box.astype("int")  # l t r b
 
-        margin = ((b-t) - (r-l))//2
-        l = l - margin if (b-t-r+l)%2 == 0 else l - margin - 1
+        original_vertical_length = b - t
+        t = int(t + (original_vertical_length) * 0.15) - border
+        b = int(b - (original_vertical_length) * 0.05) - border
+
+        margin = ((b - t) - (r - l)) // 2  # 마진 : 여백
+        l = l - margin if (b - t - r + l) % 2 == 0 else l - margin - 1
         r = r + margin
-        refined_box = [t,r,b,l]
+        refined_box = [t, r, b, l]
         list_bboxes.append(refined_box)
         list_confidence.append(confidence)
 
     ### facenet
+    # 얼굴을 인코딩해준다. 부호화(encoding) 해주는 것
     face_encodings = face_recognition.face_encodings(img_rgb, list_bboxes)
     closest_distances = knn_clf.kneighbors(face_encodings, n_neighbors=1)
     is_recognized = [closest_distances[0][i][0] <= 0.4 for i in range(len(list_bboxes))]
-    list_reconized_face = [(pred, loc, conf) if rec else ("unknown", loc, conf) for pred, loc, rec, conf in zip(knn_clf.predict(face_encodings), list_bboxes, is_recognized, list_confidence)]
-    # print (list_reconized_face)
+    list_reconized_face = [(pred, loc, conf) if rec else ("unknown", loc, conf) for pred, loc, rec, conf in
+                           zip(knn_clf.predict(face_encodings), list_bboxes, is_recognized, list_confidence)]
+    # print (list_reconized_face)        #list_reconized_face : 리스트화된 인식된 얼굴
 
     time = (cv2.getTickCount() - start) / cv2.getTickFrequency() * 1000
-    print ('%d, elapsed time: %.3fms'%(idx,time))
+    print('%d, elapsed time: %.3fms' % (idx, time))
 
     ### blurring
     img_bgr_blur = img_bgr_ori.copy()
     for name, bbox, conf in list_reconized_face:
-        t,r,b,l = bbox
+        t, r, b, l = bbox
         if name == 'unknown':
             face = img_bgr_blur[t:b, l:r]
-            small = cv2.resize(face, None, fx=.05, fy=.05, interpolation=cv2.INTER_NEAREST)
+            small = cv2.resize(face, None, fx=.07, fy=.07, interpolation=cv2.INTER_NEAREST)       # fx=.05, fy=.05 -> fx=.07, fy=.07 : 숫자 키워야 블러 픽셀 감소
             blurred_face = cv2.resize(small, (face.shape[:2]), interpolation=cv2.INTER_NEAREST)
             img_bgr_blur[t:b, l:r] = blurred_face
 
     ### draw rectangle bbox
     if args.with_draw == 'True':
+        # Image.fromarray()를사용하여, numpy 배열을 PIL Image로 변환하고, 이를 save()를 통해 저장
         source_img = Image.fromarray(img_bgr_ori)
+        # 즉, NumPy 배열을 PIL 이미지로 변환하는 코드
         draw = ImageDraw.Draw(source_img)
         for name, bbox, confidence in list_reconized_face:
-            t,r,b,l = bbox
+            t, r, b, l = bbox
             # print (int((r-l)/img_bgr_ori.shape[1]*100))
-            font_size = int((r-l)/img_bgr_ori.shape[1]*100)
+            font_size = int((r - l) / img_bgr_ori.shape[1] * 100)
 
-            draw.rectangle(((l,t),(r,b)), outline=(0,255,128))
+            draw.rectangle(((l, t), (r, b)), outline=(0, 255, 128))
 
-            draw.rectangle(((l,t-font_size-2),(r,t+2)), fill=(0,255,128))
-            draw.text((l, t - font_size), name, font=ImageFont.truetype('./BMDOHYEON_TTF.TTF', font_size), fill=(0,0,0,0))
-
+            draw.rectangle(((l, t - font_size - 2), (r, t + 2)), fill=(0, 255, 128))
+            draw.text((l, t - font_size), name, font=ImageFont.truetype('./BMDOHYEON_TTF.TTF', font_size),
+                      fill=(0, 0, 0, 0))
+        # np.asarray : 입력된 데이터를 numpy.ndarray() 형식으로 만들어줍니다.
+        # ndarray는 Numpy의 핵심인 '다차원 행렬 자료구조 클래스'
         show = np.asarray(source_img)
-        cv2.imshow('show', show)
-        cv2.imshow('blur', img_bgr_blur)
+
+        # 라벨링 영상 불러오고 저장하는 것 [추가]
+        cv2.imshow('show', show)  # 라벨링 영상 보여주는 것
+        out.write(show)  # 라벨링 한 영상 avi로 저장
+        # 모자이크 영상 불러오고 저장하는 것 [추가]
+        cv2.imshow('blur', img_bgr_blur)  # 모자이크 한 영상 보여주는 것(읽어오기)
+        out_2.write(img_bgr_blur)  # 모자이크 처리한 영상 avi로 저장
+
         key = cv2.waitKey(30)
         if key == 27:
             break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+# 동영상 파일 저장하는 원본 코드(복붙한 것)
+# cap = cv2.VideoCapture('./data/with_M.mp4')     # 사용할 동영상 넣으면 결과로 이 영상에서 모자이크처리와 라벨링 보여줌
+# 비디오 캡쳐. 동영상을 cap에 저장.
+# width = int(cap.get(3)) # 가로 길이 가져오기
+# height = int(cap.get(4)) # 세로 길이 가져오기
+# fps = 20
+#
+# fcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+# out = cv2.VideoWriter('output_blur.avi', fcc, fps, (width, height))
+# print(fps)
+# print(fcc)
+# print(width)
+# print(height)
+# while (cap.isOpened()) :
+#
+#     ret, frame = cap.read()
+#
+#     if ret :
+#         frame = cv2.flip(frame, 0)
+#         out.write(frame)
+#         cv2.imshow('frame', frame)
+#
+#         if cv2.waitKey(1) & 0xFF == ord('q') : break
+#
+#     else :
+#         print("Fail to read frame!")          # 프레임이 끝나면 이 구문이 출력
+#         break
 
 
 ### opencv text, box drawing
@@ -147,7 +211,7 @@ for idx in range(length):
 # text = "%s: %.2f" % (name,confidence)
 # text_size, base_line = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 2)
 # y = t #- 1 if t - 1 > 1 else t + 1
-# cv2.rectangle(img_bgr_ori, 
+# cv2.rectangle(img_bgr_ori,
 #             (l,y-text_size[1]),(l+text_size[0], y+base_line), (0,255,0), -1)
-# cv2.putText(img_bgr_ori, text, (l, y), 
+# cv2.putText(img_bgr_ori, text, (l, y),
 #             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
